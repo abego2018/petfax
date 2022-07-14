2ebf073d4cc9b61d28a4922eac25d92bb60a7c83
Abraham Gonzalez <abraham.gm08@gmail.com>
2022-07-13 20:56:14 -0700

    adding pet show page

--- petfax/pet.py
+++ petfax/pet.py
@@ -11,3 +11,8 @@ bp = Blueprint('pet', __name__, url_prefix="/pets")
 def index(): 
     return render_template('index.html', pets=pets)
 
+@bp.route('/show/<int:id>')
+def show(id):
+    pet = pets[id - 1]
+    return render_template('show.html', pet=pet)
+
