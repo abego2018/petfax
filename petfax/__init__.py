a6453dfb9b2b81818399887fb59e8448dc559da7
Abraham Gonzalez <abraham.gm08@gmail.com>
2022-07-15 16:09:20 -0700

    fact form showing, route working, registered bp to display

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -11,4 +11,6 @@ def create_app():
     from . import pet
     app.register_blueprint(pet.bp)
 
+    from . import fact
+    app.register_blueprint(fact.bp)
     return app
--- petfax/fact.py
+++ petfax/fact.py
@@ -2,6 +2,6 @@ from flask import (Blueprint, render_template)
 
 bp = Blueprint('fact', __name__, url_prefix="/facts")
 
-@bp.rout('/new')
+@bp.route('/new')
 def new():
     return render_template('facts/new.html')
